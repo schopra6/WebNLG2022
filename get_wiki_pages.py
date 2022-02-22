594a32d15b9774714aa7065103f81709f69a8bad
pa1007 <pa.fourriere@gmail.com>
2022-02-22 17:10:20 +0100

    refactor for better readability

--- get_wiki_pages.py
+++ get_wiki_pages.py
@@ -5,45 +5,50 @@ import wikipediaapi
 from benchmark_reader import Benchmark
 from benchmark_reader import select_files
 
-base_url = "https://wikipedia.com/wiki/"
-base_path = "wk/en/"
-base = "wk/"
 
-language = ["fr", "hi", "ru", "pt", "br"]
+def get_all_pages(pageName):
+    page = wiki_html.page(pageName)
+    if page.exists():
+        file = open(base_path + page.title + ".html", "w", encoding="utf-8")
+        file.write(page.text)
+        file.close()
 
-if not os.path.exists(base):
-    os.makedirs(base_path)
-    for x in language:
-        os.makedirs(base + x + "/")
+        for link in page.langlinks:
+            lpage = page.langlinks[link]
+            if lpage.language in language:
+                file = open(base + lpage.language + "/" + page.title + ".html", "w", encoding="utf-8")
+                file.write(lpage.text)
+                file.close()
 
-# where to find the corpus
-path_to_corpus = 'corpus/ru/train/'
 
-# initialise Benchmark object
-b = Benchmark()
+if __name__ == '__main__':
+    base_url = "https://wikipedia.com/wiki/"
+    base_path = "wk/en/"
+    base = "wk/"
 
-# collect xml files
-files = select_files(path_to_corpus)
+    language = ["fr", "hi", "ru", "pt", "br"]
+    wiki_html = wikipediaapi.Wikipedia(
+        language='en',
+        extract_format=wikipediaapi.ExtractFormat.HTML
+    )
 
-# load files to Benchmark
-b.fill_benchmark(files)
+    if not os.path.exists(base):
+        os.makedirs(base_path)
+        for x in language:
+            os.makedirs(base + x + "/")
 
-urls = []
-for entry in b.entries:
-    for x in entry.modifiedtripleset.triples:
-        wiki_html = wikipediaapi.Wikipedia(
-            language='en',
-            extract_format=wikipediaapi.ExtractFormat.HTML
-        )
-        page = wiki_html.page(x.o)
-        if page.exists():
-            file = open(base_path + page.title + ".html", "w", encoding="utf-8")
-            file.write(page.text)
-            file.close()
+    # where to find the corpus
+    path_to_corpus = 'corpus/en/train/'
+    # initialise Benchmark object
+    b = Benchmark()
 
-            for link in page.langlinks:
-                lpage = page.langlinks[link]
-                if lpage.language in language:
-                    file = open(base + lpage.language + "/" + page.title + ".html", "w", encoding="utf-8")
-                    file.write(lpage.text)
-                    file.close()
+    # collect xml files
+    files = select_files(path_to_corpus)
+
+    # load files to Benchmark
+    b.fill_benchmark(files)
+
+    urls = []
+    for entry in b.entries:
+        for x in entry.modifiedtripleset.triples:
+            get_all_pages(x.o)
