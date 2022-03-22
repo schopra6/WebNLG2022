b7bb2076a84cd2ba92091911cc259a320d715fa4
CFR2000 <colm.rooney@ucdconnect.ie>
2022-03-22 01:41:32 +0100

    Commiting files for en-fr translation

---
+++ data2/run.py
@@ -0,0 +1,19 @@
+import os
+from os import walk
+import sys
+import logging
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
+from Translation import Translate
+def main(path,src='/ru/',tgt='/fr/'):
+    f = []
+    for (dirpath, dirnames, filenames) in walk(path):
+        newdirpath=dirpath.replace(src,tgt)
+        if not os.path.exists(newdirpath):
+            os.mkdir(newdirpath)
+        for xmlfile in filenames:
+            logging.info("filename:"+xmlfile+"directory:"+newdirpath )
+            t = Translate()
+            t.create_french_benchmark2xml([dirpath+'/'+xmlfile],newdirpath,xmlfile)
+
+if __name__ == "__main__":
+    main(sys.argv[1])
