5441f31fdec0fbc35aa56c95e25afecc931eb408
pa1007 <pa.fourriere@gmail.com>
2022-04-01 10:50:25 +0200

    Fix errors

--- units_reader.py
+++ units_reader.py
@@ -22,17 +22,17 @@ class UnitReader:
     def read(self, directory):
         files = os.listdir(directory)
         for file in files:
-            f = open(directory + file)
+            f = open(directory + file,encoding="utf8")
             x = json.load(f)
             name = os.path.basename(f.name).replace(".json", '')
             self.entries.append(Unit(name, x[name]))
 
-    def getAll(self, lang):
+    def get_all(self, lang):
         res = []
         for e in self.entries:
             res.append({e.name: e.parts[lang]})
 
         return res
 
-    def getUnit(self, unit, lang):
+    def get_unit(self, unit, lang):
         return [y.parts[lang] for y in list(filter(lambda x: x.name == unit, self.entries))][0]
